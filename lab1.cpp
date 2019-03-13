564393e60e066d9f6c40dfd637e87feb7d959171
artykisa <48124997+artykisa@users.noreply.github.com>
2019-03-13 23:54:24 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -15,7 +15,6 @@ eshe: system("cls");
 		if (a[2] == '\0')  len = 2;
 		else { printf("Неправильный ввод!\n Попробуйте еще раз . . ."); system("pause"); goto eshe; }
 	}
-	if (len > 2) { printf("Неправильный ввод!\n Попробуйте еще раз . . ."); system("pause"); goto eshe; }
 	for (int i = 0; i < len; i++) {
 		if (a[i] != '0' && a[i] != '1' && a[i] != '2' && a[i] != '3'&& a[i] != '4' && a[i] != '5' && a[i] != '6'&& a[i] != '7' && a[i] != '8' && a[i] != '9') {
 			printf("Неправильный ввод!\n Попробуйте еще раз . . ."); system("pause"); goto eshe;
