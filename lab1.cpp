dc093013e1daa4479c5f6b89c6e00dbf3168432f
artykisa <48124997+artykisa@users.noreply.github.com>
2019-03-13 23:47:49 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -11,48 +11,48 @@ eshe: system("cls");
 	if (a[1] == '\0') len = 1;
 	else {
 		if (a[2] == '\0')  len = 2;
-		else { printf("Неправильный ввод!\n Попробуйте еще раз . . ."); _getch(); goto eshe; }
+		else { printf("Неправильный ввод!\n Попробуйте еще раз . . ."); system("pause"); goto eshe; }
 	}
-	if (len > 2) { printf("Неправильный ввод!\n Попробуйте еще раз . . ."); _getch(); goto eshe; }
+	if (len > 2) { printf("Неправильный ввод!\n Попробуйте еще раз . . ."); system("pause"); goto eshe; }
 	for (int i = 0; i < len; i++) {
 		if (a[i] != '0' && a[i] != '1' && a[i] != '2' && a[i] != '3'&& a[i] != '4' && a[i] != '5' && a[i] != '6'&& a[i] != '7' && a[i] != '8' && a[i] != '9') {
-			printf("Неправильный ввод!\n Попробуйте еще раз . . ."); _getch(); goto eshe;
+			printf("Неправильный ввод!\n Попробуйте еще раз . . ."); system("pause"); goto eshe;
 		}
 	}
 	if (a == "00") {
 		printf("Ноль\n") ;
-		_getch();
+		system("pause");
 		return 0;
 	}
 	if (len == 1)
 	{
 		switch(a[0]){
-		case '1':  printf("Один\n");    _getch(); return 0; break;
-		case '2':  printf("Два\n");     _getch(); return 0; break;
-		case '3':  printf("Три\n");     _getch(); return 0; break;
-		case '4':  printf("Четыре\n");  _getch(); return 0; break;
-		case '5':  printf("Пять\n");    _getch(); return 0; break;
-		case '6':  printf("Шесть\n");   _getch(); return 0; break;
-		case '7':  printf("Семь\n");    _getch(); return 0; break;
-		case '8':  printf("Восемь\n");  _getch(); return 0; break;
-		case '9':  printf("Девять\n");  _getch(); return 0; break;
-		case '0':  printf("Ноль\n");    _getch(); return 0; break;
+		case '1':  printf("Один\n");    system("pause"); return 0; 
+		case '2':  printf("Два\n");     system("pause"); return 0; 
+		case '3':  printf("Три\n");     system("pause"); return 0; 
+		case '4':  printf("Четыре\n");  system("pause"); return 0; 
+		case '5':  printf("Пять\n");    system("pause"); return 0; 
+		case '6':  printf("Шесть\n");   system("pause"); return 0; 
+		case '7':  printf("Семь\n");    system("pause"); return 0; 
+		case '8':  printf("Восемь\n");  system("pause"); return 0; 
+		case '9':  printf("Девять\n");  system("pause"); return 0; 
+		case '0':  printf("Ноль\n");    system("pause"); return 0; 
 	                }
 	}
 	if (len == 2) {
 		
 			switch (a[0]) {
 			case '1': switch (a[1]) {
-				case '1': printf("Одиннадцать\n");    _getch(); return 0; break;
-				case '2': printf("Двенадцать\n");     _getch(); return 0; break;
-				case '3':  printf("Тринадцать\n");    _getch(); return 0; break;
-				case '4':  printf("Четырнадцать\n");  _getch(); return 0; break;
-				case '5': printf("Пятнадцать\n");     _getch(); return 0; break;
-				case '6':  printf("Шестнадцать\n");   _getch(); return 0; break;
-				case '7':  printf("Семнадцать\n");    _getch(); return 0; break;
-				case '8':  printf("Восемнадцатьт\n"); _getch(); return 0; break;
-				case '9':   printf("Девятнадцать\n"); _getch(); return 0; break;
-				case '0':   printf("");               _getch(); return 0; break;
+				case '1':  printf("Одиннадцать\n");   system("pause"); return 0; 
+				case '2':  printf("Двенадцать\n");    system("pause"); return 0;
+				case '3':  printf("Тринадцать\n");    system("pause"); return 0;
+				case '4':  printf("Четырнадцать\n");  system("pause"); return 0;
+				case '5':  printf("Пятнадцать\n");    system("pause"); return 0;
+				case '6':  printf("Шестнадцать\n");   system("pause"); return 0;
+				case '7':  printf("Семнадцать\n");    system("pause"); return 0;
+				case '8':  printf("Восемнадцатьт\n"); system("pause"); return 0;
+				case '9':  printf("Девятнадцать\n");  system("pause"); return 0;
+				case '0':  printf("");                system("pause"); return 0;
 			}
 				
 			case '2':  printf( "Двадцать");   break;
@@ -66,16 +66,16 @@ eshe: system("cls");
 			case '0':  printf("");            break;
 			}
 			switch (a[1]) {
-			case '1':  printf(" Один\n");   _getch(); return 0; break;
-			case '2':  printf(" Два\n");    _getch(); return 0; break;
-			case '3':  printf(" Три\n");    _getch(); return 0; break;
-			case '4':  printf(" Четыре\n"); _getch(); return 0; break;
-			case '5':  printf(" Пять\n");   _getch(); return 0; break;
-			case '6':  printf(" Шесть\n");  _getch(); return 0; break;
-			case '7':  printf(" Семь\n");   _getch(); return 0; break;
-			case '8':  printf(" Восемь\n"); _getch(); return 0; break;
-			case '9':  printf(" Девять\n"); _getch(); return 0; break;
-			case '0':  printf(" Ноль\n");   _getch(); return 0; break;
+			case '1':  printf(" Один\n");   system("pause"); return 0; 
+			case '2':  printf(" Два\n");    system("pause"); return 0; 
+			case '3':  printf(" Три\n");    system("pause"); return 0; 
+			case '4':  printf(" Четыре\n"); system("pause"); return 0; 
+			case '5':  printf(" Пять\n");   system("pause"); return 0; 
+			case '6':  printf(" Шесть\n");  system("pause"); return 0; 
+			case '7':  printf(" Семь\n");   system("pause"); return 0; 
+			case '8':  printf(" Восемь\n"); system("pause"); return 0; 
+			case '9':  printf(" Девять\n"); system("pause"); return 0; 
+			case '0':  printf(" Ноль\n");   system("pause"); return 0; 
 			}
 	}
 	system("pause");
