5494bfcd92f822ef04b7bcab471b10606e73ada4
lubakach <48528418+lubakach@users.noreply.github.com>
2019-03-16 12:31:31 +0300

    Update Lab1.c

--- lubakach/Lab1.c
+++ lubakach/Lab1.c
@@ -4,35 +4,19 @@
 
 int main()
 {
-	int m, m1, i = 0;//m-число, m1-копия, i-индекс массива
-	int n;//значение элемента массива
-	int reshenie[20];
+	int m,i=0, reshenie[20];
+	int resh=0, chislo=1;
 	printf("Napishi chislo m?\n");
 	scanf_s("%d", &m);
-	m1 = m;//копия числа m
-	for (int y = m; y > 0; y--)
-	{
-		_Bool proverka = true;
-		m1 = y;
-		while (m1)//перевод в двоичную систему числа и запись в массив
-		{
-			reshenie[i] = (m1 & 1);
-			m1 = m1 >> 1;
-			if (reshenie[i] == 0)
-				proverka = false;
-			i++;
+	int m1 = m;
+	
+	while(chislo<20){
+		chislo = (chislo << 1) + 1;
+		if (chislo < m1) {
+			printf("\n%d", (chislo));
 		}
-		if (proverka)//проверка на выполнение условия
-		{
-			printf("\n%d-", y);
-			for (int l = 0; l < i; l++)
-				printf("%d", reshenie[i]);
-		}
-
-		i = 0;
-
-	}
-
+		
+	} 
 	return 0;
 }
 
