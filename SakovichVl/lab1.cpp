ea7cd31b183e4c3e54e3e357c6392b8c6dbfa18f
SakovichVl <48560283+SakovichVl@users.noreply.github.com>
2019-03-16 11:46:38 +0300

    Add files via upload

---
+++ SakovichVl/lab1.cpp
@@ -0,0 +1,60 @@
+//Вариант 25
+//Сакович Влад
+#include "pch.h"
+#include <iostream>
+
+int HowMany(int a)
+{
+	int b = 0;
+	while (a)
+	{
+		a /= 2;
+		b++;
+	}
+	return b;
+}
+void perevod(int *arr, int a, int num)
+{
+
+	for (int i = 0; i < a; i++)
+	{
+		arr[i] = num % 2;
+		num /= 2;
+	}
+	for (int i = 0; i < a; i++) {
+	}
+}
+void out(int num, int *arr, int a)
+{
+	int ultra = 0, b = a - 1, a1 = a / 2;
+	for (int i = 0; i < a1; i++)
+	{
+		if (arr[i] != arr[b]) {
+			ultra = 1;
+		}
+		b--;
+	}
+	if (ultra == 0)
+	{
+		printf("%d  ", num);
+		for (int i = 0; i < a; i++) {
+			printf("%d", arr[i]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int m, kolvo;
+	printf("Input m\n");
+	scanf_s("%d", &m);
+	for (int i = 1; i < m; i++)
+	{
+		kolvo = HowMany(i);
+		int *arr = new int[kolvo];
+		perevod(arr, kolvo, i);
+		out(i, arr, kolvo);
+		delete[] arr;
+	}
+}
