681ca42305b24f2b94f85e79e18f144a823ae568
artykisa <48124997+artykisa@users.noreply.github.com>
2019-05-18 14:27:27 +0300

    Add files via upload

---
+++ artykisa/task4.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+	int S, F, K;
+	printf("Vvedite 3 chisla\n");
+	scanf_s("%d%d%d", &S, &F, &K);	
+	int schet=0;
+	int dlina;
+	char itochar[12];
+	for (S; S <= F; S++) {
+		sprintf_s(itochar, "%d", S);
+		char temp;
+		for (int i = 0; i < strlen(itochar) / 2; i++)
+		{
+			temp = itochar[i];
+			itochar[i] = itochar[strlen(itochar) - 1 - i];
+			itochar[strlen(itochar) - 1 - i] = temp;			
+		}
+		if (abs(atoi(itochar) - S) <= K) {
+			schet++;
+		}
+	}
+	printf("Kol-vo takix chisel=%d",schet);
+	return 0;
+}
+  
+
+
